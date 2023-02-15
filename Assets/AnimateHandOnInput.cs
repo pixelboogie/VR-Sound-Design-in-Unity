f51917ae4eb6f5d66d6e802dac7149b06882541f
pixelboogie <justamovie@yahoo.com>
2023-02-15 06:58:17 -0600

    Cleaned up code

--- Assets/AnimateHandOnInput.cs
+++ Assets/AnimateHandOnInput.cs
@@ -10,14 +10,6 @@ public class AnimateHandOnInput : MonoBehaviour
 
     public Animator handAnimator;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         float triggerValue = pinchAnimationAction.action.ReadValue<float>();
